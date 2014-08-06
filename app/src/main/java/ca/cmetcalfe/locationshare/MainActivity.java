38253a9f15c776db11b53f0face3ea083bf1e06e
pR0Ps <pR0Ps.CM@gmail.com>
2014-08-05 22:59:24 -0400

    Changed to using anonymous classes, added a GpsStatus listener

--- app/src/main/java/ca/cmetcalfe/locationshare/MainActivity.java
+++ app/src/main/java/ca/cmetcalfe/locationshare/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.GpsStatus;
 import android.net.Uri;
 import android.os.Handler;
 import android.view.View;
@@ -18,10 +19,12 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    LocationManager mlocManager;
-    LocationListener mlocListener;
-    Location location;
+    LocationManager mLocManager;
+    LocationListener mLocListener;
+    GpsStatus.Listener gpsStatusListener;
+    Location lastLocation;
     long lastFix;
+    boolean hasFix;
 
     private Handler handler = new Handler();
     private Runnable updateUITask = new Runnable() {
@@ -36,14 +39,69 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        mlocListener = new MyLocationListener(this);
-        location = mlocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location != null){
-            lastFix = location.getTime();
-        }
+        final Context context = getApplicationContext();
 
-        handler.post(updateUITask);
+        hasFix = false;
+
+        mLocManager = (LocationManager)context.getSystemService(LOCATION_SERVICE);
+        boolean isGPSEnabled = mLocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (isGPSEnabled && mLocManager != null){
+
+            // Create the LocationListener
+            mLocListener = new LocationListener(){
+                public void onLocationChanged(Location loc){
+                    updateLocation(loc);
+                }
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+                public void onProviderEnabled(String provider) {}
+                public void onProviderDisabled(String provider) {}
+            };
+
+            // Create the GPS status listener
+            gpsStatusListener = new GpsStatus.Listener(){
+                public void onGpsStatusChanged(int event){
+                    switch(event){
+                    case GpsStatus.GPS_EVENT_STARTED:
+                         System.out.println("GPS_EVENT_STARTED");
+                        break;
+                    case GpsStatus.GPS_EVENT_STOPPED:
+                        System.out.println("GPS_EVENT_STOPPED");
+                        break;
+                    case GpsStatus.GPS_EVENT_FIRST_FIX:
+                        System.out.println("GPS_EVENT_FIRST_FIX");
+                        hasFix = true;
+                        break;
+                    case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                        System.out.println("GPS_EVENT_SATELLITE_STATUS");
+                        if (lastLocation != null) {
+                            hasFix = (System.currentTimeMillis() - lastFix) < 2000;
+                        }
+
+                        if (hasFix) {
+                            System.out.println("HAS FIX");
+                            }
+                        else {
+                            System.out.println("NO FIX");
+                        }
+                    }
+                }
+            };
+
+            // Register GPSStatus listener for events
+            mLocManager.addGpsStatusListener(gpsStatusListener);
+
+
+
+            mLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, mLocListener);
+
+            lastLocation = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null){
+                lastFix = lastLocation.getTime();
+            }
+
+            handler.post(updateUITask);
+        }
     }
 
     @Override
@@ -56,22 +114,22 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onStop(){
         super.onStop();
-        mlocManager.removeUpdates(mlocListener);
+        mLocManager.removeUpdates(mLocListener);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
-        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mlocListener);
+        mLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocListener);
+        mLocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocListener);
     }
 
     public void shareLocation (View view){
-        if (this.location == null){
+        if (lastLocation == null){
             return;
         }
 
-        String link = formatLocation("http://maps.google.com/?q={0},{1}", this.location);
+        String link = formatLocation("http://maps.google.com/?q={0},{1}", lastLocation);
 
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
@@ -81,11 +139,11 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void viewLocation (View view){
-        if (this.location == null){
+        if (lastLocation == null){
             return;
         }
 
-        String uri = formatLocation("geo:{0},{1}?q={0},{1}", this.location);
+        String uri = formatLocation("geo:{0},{1}?q={0},{1}", lastLocation);
 
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
         startActivity(Intent.createChooser(intent, getResources().getText(R.string.view_via)));
@@ -93,20 +151,20 @@ public class MainActivity extends ActionBarActivity {
 
     public void updateLocation(Location location){
         //TODO: Check if new location is 'better'
-        this.location = location;
-        this.lastFix = location.getTime();
+        lastLocation = location;
+        lastFix = location.getTime();
         updateDisplay();
     }
 
     public void updateDisplay(){
 
-        if (this.location != null){
-            long timeElapsed = System.currentTimeMillis() - this.lastFix;
+        if (lastLocation != null){
+            long timeElapsed = System.currentTimeMillis() - lastFix;
             ((TextView)findViewById(R.id.timeText)).setText(stringifyTime(timeElapsed/1000));
 
-            ((TextView)findViewById(R.id.latitudeText)).setText("" + this.location.getLatitude());
-            ((TextView)findViewById(R.id.longitudeText)).setText("" + this.location.getLongitude());
-            ((TextView)findViewById(R.id.accuracyText)).setText(new DecimalFormat("###.00").format(this.location.getAccuracy()) + "m");
+            ((TextView)findViewById(R.id.latitudeText)).setText("" + lastLocation.getLatitude());
+            ((TextView)findViewById(R.id.longitudeText)).setText("" + lastLocation.getLongitude());
+            ((TextView)findViewById(R.id.accuracyText)).setText(new DecimalFormat("###.00").format(lastLocation.getAccuracy()) + "m");
         }
         else{
             ((TextView)findViewById(R.id.latitudeText)).setText("N/A");
@@ -118,20 +176,18 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private String formatLocation(String s, Location l){
-        //Hack to get around MessageFormat precision weirdness
+        // Hack to get around MessageFormat precision weirdness
         return MessageFormat.format(s, ""+l.getLatitude(), ""+l.getLongitude(), ""+l.hasAccuracy());
     }
 
     public static String stringifyTime(long seconds) {
 
-        //int days = (int)(seconds/86400);
-        //seconds -= days * 86400;
         int hours = (int)(seconds/3600);
         seconds -= hours * 3600;
         int minutes = (int)(seconds/60);
         seconds -= minutes * 60;
 
-        //Fix negative values (rounding)
+        // Fix negative values (rounding)
         seconds = Math.max(0, seconds);
 
         return MessageFormat.format("{0,number,00}:{1,number,00}:{2,number,00}", hours, minutes, seconds);
