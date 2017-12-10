549cdfcf10cdca224ddfc552464b6bf89d20750f
mahula <lenzmath@gmail.com>
2017-12-10 17:15:11 +0100

    Bind preference summary to value

--- app/src/main/java/ca/cmetcalfe/locationshare/SettingsActivity.java
+++ app/src/main/java/ca/cmetcalfe/locationshare/SettingsActivity.java
@@ -1,9 +1,28 @@
 package ca.cmetcalfe.locationshare;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
+
+	private static Preference.OnPreferenceChangeListener prefsListener =
+            new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference pref, Object value) {
+                    String valueString = value.toString();
+
+                    if (pref instanceof ListPreference) {
+                        ListPreference listPreference = (ListPreference) pref;
+                        int index = listPreference.findIndexOfValue(valueString);
+
+                        pref.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+                    }
+                    return true;
+                }
+            };
 
     // ----------------------------------------------------
     // Android Lifecycle
@@ -13,5 +32,18 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+        bindPreferenceSummaryToValue(findPreference("prefLinkType"));
+    }
+
+    //-----------------------------------------------------
+    // Preferences related methods
+    //-----------------------------------------------------
+    private static void bindPreferenceSummaryToValue(Preference pref) {
+        pref.setOnPreferenceChangeListener(prefsListener);
+
+        prefsListener.onPreferenceChange(pref,
+                                         PreferenceManager
+                                                 .getDefaultSharedPreferences(pref.getContext())
+                                                 .getString(pref.getKey(), ""));
     }
 }
